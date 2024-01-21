85c768af3529cf04aef510e55f21a798a924eec6
zakaWAC <129171507+zakaWAC@users.noreply.github.com>
2024-01-21 19:25:06 +0100

    add types

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 import { SidebarLink } from "@/types";
+
 export const themes = [
   { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
   { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
